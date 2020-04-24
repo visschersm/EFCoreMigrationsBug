3b278358792fd3b053296ff735fbae6045c3b91a
Mark Visschers <visschers.m@gmail.com>
2020-04-24 10:03:01 +0200

    Add project files.

---
+++ MyContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreMigrationTests
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<MyEntity> MyEntities { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=TodoContext;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            //if (!optionsBuilder.IsConfigured)
+            //{
+            //    optionsBuilder.UseInMemoryDatabase("TodoContext");
+            //}
+
+            //base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ MyEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace EFCoreMigrationTests
+{
+    public class MyEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
